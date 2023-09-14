8b43ebcca2f337689d6df5dedd764f99fe4b3964
Pat Welch <mousebrains@users.noreply.github.com>
2023-09-14 22:38:47 +1000

    Check if JAC_T exists

--- Code/add_seawater_properties.m
+++ Code/add_seawater_properties.m
@@ -17,10 +17,12 @@ lat(isnan(lat)) = latDefault;
 lon(isnan(lon)) = lonDefault;
 
 slow = profile.slow;
-slow.JAC_SP = gsw_SP_from_C(slow.JAC_C, slow.JAC_T, slow.P_slow); % Practical salinity
-slow.JAC_SA = gsw_SA_from_SP(slow.JAC_SP, slow.P_slow, profile.lon, profile.lat); % Absolute salinity
-slow.JAC_theta = gsw_CT_from_t(slow.JAC_SA, slow.JAC_T, slow.P_slow); % Conservation T
-slow.JAC_sigma = gsw_sigma0(slow.JAC_SA, slow.JAC_theta);
-slow.JAC_rho = gsw_rho(slow.JAC_SA, slow.JAC_theta, slow.P_slow) - 1000; % density kg/m^3 - 1000
-slow.depth = gsw_depth_from_z(gsw_z_from_p(slow.P_slow, profile.lat));
+if ismember("JAC_T", slow.Properties.VariableNames)
+    slow.JAC_SP = gsw_SP_from_C(slow.JAC_C, slow.JAC_T, slow.P_slow); % Practical salinity
+    slow.JAC_SA = gsw_SA_from_SP(slow.JAC_SP, slow.P_slow, lon, lat); % Absolute salinity
+    slow.JAC_theta = gsw_CT_from_t(slow.JAC_SA, slow.JAC_T, slow.P_slow); % Conservation T
+    slow.JAC_sigma = gsw_sigma0(slow.JAC_SA, slow.JAC_theta);
+    slow.JAC_rho = gsw_rho(slow.JAC_SA, slow.JAC_theta, slow.P_slow) - 1000; % density kg/m^3 - 1000
+end % if ismember
+slow.depth = gsw_depth_from_z(gsw_z_from_p(slow.P_slow, lat));
 end % addSeawaterProperties
