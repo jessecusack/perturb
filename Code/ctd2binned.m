9b9fef2b2ff6112470f22813afb5dde8afda9c4f
Pat Welch <mousebrains@users.noreply.github.com>
2023-10-21 19:18:29 -0700

    Deal with ctd binning and GPS fixes better

--- Code/ctd2binned.m
+++ Code/ctd2binned.m
@@ -3,25 +3,28 @@
 %
 % July-2023, Pat Welch, pat@mousebrains.com
 
-function [row, retval, gps] = ctd2binned(row, mat, pars, gps)
+function [row, retval, mat, gps] = ctd2binned(row, mat, pars, latitude_default)
 arguments (Input)
     row table % row to work on
     mat struct % Output of mat2profile
     pars struct % Parameters, defaults from get_info
-    gps % empty or GPS_base_class
+    latitude_default double {mustBeInRange(latitude_default, -90, 90)} = 0
 end % arguments Input
 arguments (Output)
     row table % row worked on
     retval (2,1) cell % (filename ormissing) and (binned or empty)
+    mat struct % Output of mat2profile
     gps % empty GPS_base_class
 end % arguments Output
+
+gps = []; % Maybe initialized, if needed
 
 fnCTD = fullfile(pars.ctd_root, append(row.name, ".mat"));
 row.fnCTD = fnCTD;
 
 if isnewer(fnCTD, row.fnMat)
     retval = {fnCTD, []}; % Filename of the data
-    fprintf("%s: %s is newer than %s\n", row.name, row.fnMat, row.fnCTD);
+    fprintf("%s: %s is newer than %s\n", row.name, row.fnCTD, row.fnMat);
     return;
 end
 
@@ -74,16 +77,34 @@ end % if ~isempty tblFast
 
 binned.t = t0 + seconds(binned.t);
 
-if isempty(gps) % Only initialize GPS if needed
-    gps = pars.gps_class.initialize();
-end % if isempty gps
+gps = pars.gps_class.initialize(); % initialize GPS
 
-binned.lat = gps.lat(binned.t);
-binned.lon = gps.lon(binned.t);
-binned.dtGPS = gps.dt(binned.t);
+if isequal(pars.profile_direction, "down") % Check for profiles for tow-yo
+    indicesSlow = get_profile( ...
+        mat.P_slow, mat.W_slow, ...
+        pars.profile_pressure_min, ...
+        pars.profile_speed_min, ...
+        char(pars.profile_direction), ...
+        pars.profile_min_duration, ...
+        mat.fs_slow); % This will be redone in mat2profile, but this keeps it separate
+
+    if isempty(indicesSlow) % No profiles
+        binned.lat = gps.lat(binned.t);
+        binned.lon = gps.lon(binned.t);
+        binned.dtGPS = gps.dt(binned.t);
+    else % Profiles
+        t0 = datetime(append(mat.date, " ", mat.time));
+        tSlow = t0 + seconds(mat.t_slow(indicesSlow));
+        binned = addGPS(binned, tSlow, gps);
+    end % profiles
+else % Not down
+    binned.lat = gps.lat(binned.t);
+    binned.lon = gps.lon(binned.t);
+    binned.dtGPS = gps.dt(binned.t);
+end % if direction
 
 lat = binned.lat;
-lat(isnan(lat)) = 0;
+lat(isnan(lat)) = latitude_default;
 
 binned.depth = gsw_depth_from_z(gsw_z_from_p(binned.pressure, lat));
 
@@ -147,10 +168,10 @@ try
 binned.(append("n", suffix))(iLHS) = a.GroupCount(iRHS);
 binned(iLHS, cNames) = a(iRHS, cNames);
 catch ME
-    a(IRHS,:)
-    binned(iLHS,:)
-    cNames
-    suffix
+    disp(a(IRHS,:)); 
+    disp(binned(iLHS,:));
+    disp(cNames);
+    disp(suffix);
     rethrow(ME)
 end % try
 end % binTable
@@ -159,4 +180,65 @@ function varargout = myMedian(varargin)
 mu = cellfun(@(x) median(x, "omitnan"), varargin, "UniformOutput", false);
 sigma = cellfun(@(x) std(x, "omitnan"), varargin(2:end), "UniformOutput", false);
 varargout = [mu, sigma];
-end % myFun
+end % myMedian
+
+function ctd = addGPS(ctd, tSlow, gps)
+arguments (Input)
+    ctd table
+    tSlow (2,:) datetime
+    gps GPS_base_class
+end % arguments Input
+arguments (Output)
+    ctd table
+end % arguments Output
+
+ctd.lon = nan(size(ctd.t)); % Preallocate
+ctd.lat = nan(size(ctd.t));
+ctd.dtGPS = nan(size(ctd.t));
+
+% Before the first cast, assume the fix is at ctd.t
+q = ctd.t < tSlow(1,1); % Before first profile
+if any(q)
+    t = ctd.t(q);
+    ctd.lon(q) = gps.lon(t);
+    ctd.lat(q) = gps.lat(t);
+    ctd.dtGPS(q) = gps.dt(t);
+end % if any q
+
+
+nProfiles = size(tSlow,2);
+
+for index = 1:nProfiles % Walk through each cast
+    t = tSlow(1, index); % Start of the cast
+    q = ctd.t >= t & ctd.t <= tSlow(2,index);
+    ctd.lon(q) = gps.lon(t);
+    ctd.lat(q) = gps.lat(t);
+    ctd.dtGPS(q) = gps.dt(t);
+end % for
+
+for index = 1:nProfiles % Gaps between casts
+    t0 = tSlow(2,index); % Start of the gap
+    if index == nProfiles % All the way to the end
+        t1 = ctd.t(end);
+    else % between casts
+        t1 = tSlow(1,index+1);
+    end % if index
+
+    q = ctd.t > t0 & ctd.t < t1; % Points in the gap between casts
+    if ~any(q), continue; end % Nothing to be done here
+    ii = find(q); % Get indices
+
+    lon0 = ctd.lon(ii(1)-1); % GPS fix for the down cast
+    lat0 = ctd.lat(ii(1)-1);
+    dt = seconds(t1 - t0); % Time from start of being reeled in until reeled in
+    dLon = gps.lon(t0) - lon0; % Lon difference between cast and ship fix at end of cast
+    dLat = gps.lat(t0) - lat0;
+    dLondt = dLon / dt; % Rate of change cast
+    dLatdt = dLat / dt;
+
+    t = ctd.t(q);
+    dt = seconds(t1 - t);
+    ctd.lon(q) = gps.lon(t) - dLondt .* dt;
+    ctd.lat(q) = gps.lat(t) - dLatdt .* dt;
+end
+end % addGPS
