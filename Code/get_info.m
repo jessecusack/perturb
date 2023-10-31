f63edc56f0f702481f561072fd2d3807aa5d7480
Pat Welch <mousebrains@users.noreply.github.com>
2023-10-30 17:08:49 -0700

    Change diss_epsilon_minimum from 3e-10 to 1e-13, more appropriate unless there is a problem.

--- Code/get_info.m
+++ Code/get_info.m
@@ -89,7 +89,7 @@ addParameter(p, "diss_backwards_length_fac", 2, validPositive); % Multiples fft_
 addParameter(p, "diss_T1_norm", 1, validPositive); % Value to multiple T1_fast temperature probe by to calculate mean for dissipation estimate
 addParameter(p, "diss_T2_norm", 1, validPositive); % Value to multiple T2_fast temperature probe by to calculate mean for dissipation estimate
 addParameter(p, "diss_warning_fraction", 0.1); % When to warn about difference of e probes > diss_warning_ratio
-addParameter(p, "diss_epsilon_minimum", 3e-10, validPositive); % Dissipation estimates less than this are set to nan, for bad electronics
+addParameter(p, "diss_epsilon_minimum", 1e-13, validPositive); % Dissipation estimates less than this are set to nan, for bad electronics
 %% Binning parameters
 addParameter(p, "bin_method", "mean", @(x) ismember(x, ["median", "mean"])); % Which method to use to combine bins together
 addParameter(p, "bin_width", 1, validPositive); % Bin width in (m)
