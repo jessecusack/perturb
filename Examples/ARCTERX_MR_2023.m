5739ddd632b04dbb1354579eaf6a21a747379cf1
Pat Welch <mousebrains@users.noreply.github.com>
2023-10-30 18:10:58 -0700

    Change fft parameters

--- Examples/ARCTERX_MR_2023.m
+++ Examples/ARCTERX_MR_2023.m
@@ -1,5 +1,5 @@
 %
-% Convert test .P files to binned data
+% Convert ARCTERX 2023 MR .P files to binned data
 %
 % August-2023, Pat Welch, pat@mousebrains.com
 
@@ -13,7 +13,7 @@ google_root = "~/Google Drive/Shared drives/GliderGroup/ARCTERX/IOP-2023-gliders
 data_root = fullfile(google_root, "glider-offload", sprintf("OSU%s", glider), "MR");
 
 p_file_root = data_root;
-output_root = append("~/Desktop/ARCTERX/2023 IOP/tpw_MR/", glider);
+output_root = fullfile("~/Desktop/ARCTERX/2023 IOP/tpw_MR", glider);
 
 addpath(code_root, "-begin"); % Before reference to GPS_from_mat
 
@@ -21,17 +21,18 @@ GPS_filename = fullfile(google_root, "tpw", sprintf("OSU%s.gps.mat", glider));
 GPS_class = GPS_from_mat(GPS_filename, missing);
 
 process_P_files( ...
+    "bin_width", 1, ...
     "debug", true, ...
     "p_file_root", p_file_root, ... % Where the input .P files are located
     "output_root", output_root, ... % Where to write output to
     "gps_class", GPS_class, ... % Class to supply GPS data
     "p_file_merge", false, ... % Don't attempt to merge p files for MR
     "profile_direction", "up", ... % Profiles going from bottom to top
-    "profile_speed_min", 0.1, ... % Vertical speed cutoff
+    "profile_speed_min", 0.05, ... % Vertical speed cutoff
     "fp07_calibration", false, ... % Don't calibrate FP07s
     "CT_has", false, ... % Doesn't have CT information
-    "diss_forwards_fft_length_sec", 1, ... % 1 second FFT lengths (This is really forward in time)
-    "diss_forwards_length_fac", 10, ... % 10 overlaps, so at 0.1m/s -> 1 m
+    "diss_forwards_fft_length_sec", 0.5, ... % 1 second FFT lengths (This is really forward in time)
+    "diss_forwards_length_fac", 5, ... % 10 overlaps, so at 0.1m/s -> 1 m
     "diss_epsilon_minimum", 1e-13, ...   % Drop dissipation estimates smaller than this value
     "trim_use", false, ... % Don't trim at the top
     "netCDF_contributor_name", "Pat Welch", ...
