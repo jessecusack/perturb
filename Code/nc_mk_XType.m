9c8d1dc3974ddf0d7411705efd1c0397a09db98f
Pat Welch <mousebrains@users.noreply.github.com>
2023-11-13 12:21:39 -0800

    Add more data types

--- Code/nc_mk_XType.m
+++ Code/nc_mk_XType.m
@@ -17,6 +17,18 @@ switch class(val)
         x = netcdf.getConstant("NC_STRING");
     case "logical"
         x = netcdf.getConstant("NC_UBYTE");
+    case "uint16"
+        x = netcdf.getConstant("NC_USHORT");
+    case "int16"
+        x = netcdf.getConstant("NC_SHORT");
+    case "uint32"
+        x = netcdf.getConstant("NC_UINT");
+    case "int32"
+        x = netcdf.getConstant("NC_INT");
+    case "uint64"
+        x = netcdf.getConstant("NC_UINT64");
+    case "int64"
+        x = netcdf.getConstant("NC_INT64");
     otherwise
         error("Unrecognized type %s", class(val));
 end % switch
